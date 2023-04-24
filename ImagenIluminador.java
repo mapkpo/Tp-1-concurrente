bae1fc22f2bc02e660c489a30f9f1ea21b383fa6
AgustinSileoniUNC <agustinsileoni@mi.unc.edu.ar>
2023-04-24 02:09:23 -0300

    Cambios en el log, la forma de recolectar los datos y el cargador de imagenes.

--- ImagenIluminador.java
+++ ImagenIluminador.java
@@ -1,9 +1,8 @@
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class ImagenIluminador implements Runnable {
-    private Contenedor contenedor;
-    private int numerohilo;
+    private final Contenedor contenedor;
+    private final int numerohilo;
     private int cantidadIluminadas;
 
     public ImagenIluminador(Contenedor contenedor, int hilo) {
@@ -16,18 +15,27 @@ public class ImagenIluminador implements Runnable {
     public void run() {
 
         System.out.printf("%s inicializado\n", Thread.currentThread().getName());
-        while (cantidadIluminadas < contenedor.getTotalSize() || contenedor.isCargando()) {
+        while (contenedor.getIluminacionMejorada() < contenedor.getImagenes_totales() || contenedor.isCargando()) {
 
             // Obtener un objeto Imagen al azar del contenedor
-            if(contenedor.getSize() > 0) {
-                Imagen imagen;
-                imagen = contenedor.getImagenRandom();
-                if (!imagen.isIluminacionMejoradaBy(numerohilo)) {
-                    imagen.iluminar(numerohilo); //ilumina la imagen
-                    cantidadIluminadas++;
-                    //System.out.printf("Se ilumino una imagen %b \n", imagen.isIluminacionMejoradaBy(numerohilo));
+
+
+            Imagen imagen;
+            imagen = contenedor.getImagenRandom();
+            if(!imagen.isIluminacionMejoradaBy(numerohilo)){
+                imagen.iluminar(numerohilo); //ilumina la imagen
+                cantidadIluminadas++;
+                //System.out.printf("Se ilumino una imagen %b \n", imagen.isIluminacionMejoradaBy(numerohilo));
+                if(imagen.isIluminadaMejorada()){
+                    contenedor.incrementIluminacionMejorada();
                 }
-                imagen.liberar();
+            }
+
+            imagen.liberar();
+            try{
+                TimeUnit.MILLISECONDS.sleep(5);
+            } catch(InterruptedException e){
+                e.printStackTrace();
             }
                 try {
                     TimeUnit.MILLISECONDS.sleep(5);
@@ -36,7 +44,7 @@ public class ImagenIluminador implements Runnable {
                 }
             //System.out.printf("Se ilumino una imagen " + imagen.isIluminacionMejoradaBy(numerohilo));
         }
-        //System.out.println("Se iluminaron: " + cantidadIluminadas + " imagenes, por el hilo: " + numerohilo);
+        //System.out.println("Se iluminaron: " + cantidadIluminadas + "imagenes, por el hilo: " + numerohilo);
         System.out.printf("%s: Ilumino %d imagenes\n", Thread.currentThread().getName(), cantidadIluminadas);
     }
 
